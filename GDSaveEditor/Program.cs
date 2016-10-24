e3d97e1b61c6aff8a15baf53565a25e588992957
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-10-24 21:54:50 +0800

    Initial commit
    Now able to enumerate save file directories

--- GDSaveEditor/Program.cs
+++ GDSaveEditor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,44 @@ namespace GDSaveEditor
     {
         static void Main(string[] args)
         {
+            // Find all the save files!
+            var saveFileDirs = findSaveFileDirs();
+
+            // Display all save files
+            foreach (var saveFileDir in saveFileDirs)
+                Console.WriteLine(saveFileDir);
+        }
+
+        // Get back a list of grim dawn save directories that appears to have a player.gdc file
+        static List<string> findSaveFileDirs()
+        {
+            var saveFiles = new List<string>();
+
+            // Locate the Steam user data directory
+            string progdir = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            string cloudSavePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Steam\\userdata\\");
+            if (!Directory.Exists(cloudSavePath))
+                return saveFiles;
+
+            // Iterate through all found account directories
+            List<string> accountDirs = new List<string>(Directory.EnumerateDirectories(cloudSavePath));
+            foreach (var dir in accountDirs)
+            {
+                // Ignore account directories without the grim dawn remote save directory
+                string saveFileDir = Path.Combine(dir, "219990\\remote\\save\\main");
+                if (!Directory.Exists(saveFileDir))
+                    continue;
+
+                List<string> characterDirs = new List<string>(Directory.EnumerateDirectories(saveFileDir));
+                foreach (var characterDir in characterDirs)
+                {
+                    if (!File.Exists(Path.Combine(characterDir, "player.gdc")))
+                        continue;
+
+                    saveFiles.Add(characterDir);
+                }
+            }
+            return saveFiles;
         }
     }
 }
