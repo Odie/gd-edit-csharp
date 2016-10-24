cd0adecd20e930b44bfac5d4dfac92dd11d73105
Jonathan Shieh <jonathan.shieh@gmail.com>
2016-10-24 23:20:25 +0800

    Basic repl in place

--- GDSaveEditor/Program.cs
+++ GDSaveEditor/Program.cs
@@ -7,16 +7,106 @@ using System.Threading.Tasks;
 
 namespace GDSaveEditor
 {
+    public static class Extensions
+    {
+        public static void Each<T>(this IEnumerable<T> ie, Action<T, int> action)
+        {
+            var i = 0;
+            foreach (var e in ie) action(e, i++);
+        }
+    }
+
+    class ActionItem
+    {
+        public ActionItem(string selectorIn, string textIn, Action actionIn)
+        {
+            selector = selectorIn;
+            text = textIn;
+            action = actionIn;
+        }
+
+        public string selector;
+        public string text;
+        public Action action;
+    }
+
+    class Globals
+    {
+        public static List<ActionItem> activeActionMap;
+        public static string activeCharacterFile;
+    }
+
     class Program
     {
-        static void Main(string[] args)
+
+        static void saveFileSelectionScreen()
         {
+            Console.WriteLine("The following characters were found. Please select one to edit.");
+
             // Find all the save files!
             var saveFileDirs = findSaveFileDirs();
 
-            // Display all save files
-            foreach (var saveFileDir in saveFileDirs)
-                Console.WriteLine(saveFileDir);
+            // Build an action map with all character directory entries
+            var actionMap = new List<ActionItem>();
+
+            saveFileDirs.Each((saveFileDir, idx) =>
+            {
+                actionMap.Add(new ActionItem(
+                    (idx+1).ToString(), 
+                    saveFileDir.Substring(saveFileDir.LastIndexOf("_")), 
+                    () => { Globals.activeCharacterFile = saveFileDir; }));
+            });
+
+            Globals.activeActionMap = actionMap;
+        }
+
+
+        // Print whatever is in the action map
+        static void printActiveActionMap()
+        {  
+            foreach (var action in Globals.activeActionMap)
+            {
+                Console.WriteLine("{0}) {1}", action.selector, action.text);
+            }
+        }
+
+        static void processInput()
+        {
+            var input = Console.ReadLine();
+
+            // Try to look up the input in the action map
+            foreach(var action in Globals.activeActionMap)
+            {
+                // If we found an action with a matching selector...
+                if(input == action.selector && action.action != null)
+                {
+                    // Run the action now
+                    action.action();
+                    return;
+                }
+            }
+
+            // Otherwise, try to deal with it as an application input
+            if (input.ToLower() == "exit")
+                Environment.Exit(0);
+
+            // If we can't deal with it, print an error
+            Console.WriteLine("Couldn't make heads or tails out of that one. Try again?");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            saveFileSelectionScreen();
+
+            while(true)
+            {
+                printActiveActionMap();
+                Console.WriteLine();
+                Console.Write("> ");
+                processInput();
+            }
+            
         }
 
         // Get back a list of grim dawn save directories that appears to have a player.gdc file
